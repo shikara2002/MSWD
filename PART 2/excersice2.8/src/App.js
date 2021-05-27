ae65f7698126be2088f9283a0cb9849396aeabad
shikara2002 <74001420+shikara2002@users.noreply.github.com>
2021-05-27 19:31:07 +0530

    Add files via upload

---
+++ PART 2/excersice2.8/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    console.log(personObject)
+    const all_names = persons.map(person => person.name)
+    if (all_names.includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase()))
+  const row_names = () => personsToShow.map(person => 
+    <p key={person.name}>{person.name} {person.number}</p>
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <>{row_names()}</>
+    </div>
+  )
+}
+
+
+export default App;
