e213463b3b8efd6a72d6d58ed1efedd673f9df70
shikara2002 <74001420+shikara2002@users.noreply.github.com>
2021-05-30 13:35:32 +0530

    Add files via upload

---
+++ PART 2/excersice 2.14/src/Components/CountryList.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import CountryData from "./CountryData";
+
+const CountryList = ({ countries, newFilter, setFilterUsingButton }) => {
+  let filteredList = countries.filter(country =>
+    country.name.toUpperCase().includes(newFilter.toUpperCase())
+  );
+  let listLength = filteredList.length;
+  console.log(filteredList);
+
+  if (newFilter.length === 0 || listLength === 0) {
+    return <div />;
+  }
+
+  if (listLength >= 10) {
+    return <div>Too many matches, specify another filter</div>;
+    // return <div>hi</div>;
+  }
+
+  if (listLength > 1) {
+    return filteredList.map(country => (
+      <div key={country.name}>
+        {country.name}{" "}
+        <button value={country.name} onClick={setFilterUsingButton}>
+          {" "}
+          show{" "}
+        </button>
+      </div>
+    ));
+  } else {
+    return (
+      <div>
+        <CountryData country={filteredList[0]} />
+      </div>
+    );
+  }
+};
+
+export default CountryList;
